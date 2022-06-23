d4401fd320be8b2ce16e41997cd176f69692faf8
Sahildholpuria <dholpuria1999@gmail.com>
2022-06-23 13:45:52 +0530

    some minor changes

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import image from './images/404.gif'
+import image from './images/404.svg'
 
 const NoPage = () => {
     return(
         <>
-        <div>
+        <div class="md:ml-auto md:mr-auto flex flex-wrap items-center justify-center">
             <img src={image} alt="404" />
         </div>
         </>
