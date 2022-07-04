d735ef058b633b4abe696a9df492bd0d1abff18d
Sahildholpuria <dholpuria1999@gmail.com>
2022-07-04 12:37:47 +0530

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,10 +8,10 @@ const Home = () => {
         <section class="text-gray-200 body-font">
   <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Get Started With Your Next Project
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Start Your Next Project
         <br class="hidden lg:inline-block"/>
       </h1>
-      <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <p class="mb-8 leading-relaxed">We build applications that accurately solves your business needs.</p>
       <div class="flex justify-center">
         <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Get Start Now</button>
         {/* <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button> */}
