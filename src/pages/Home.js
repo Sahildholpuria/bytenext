9ef70b33cf2a57c3e80e50defbcb69994d5134c3
Sahildholpuria <dholpuria1999@gmail.com>
2022-07-04 12:03:48 +0530

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,12 @@ import home1 from './images/1.svg'
 const Home = () => {
     return (
         <>
+        <div>
         <section class="text-gray-200 body-font">
   <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Before they sold out
-        <br class="hidden lg:inline-block"/>readymade gluten
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Get Started With Your Next Project
+        <br class="hidden lg:inline-block"/>
       </h1>
       <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
       <div class="flex justify-center">
@@ -21,6 +22,7 @@ const Home = () => {
     </div>
   </div>
 </section>
+        </div>
         </>
     );
   };
